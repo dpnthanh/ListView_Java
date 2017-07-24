535291c83181b2f3a15209c9964f4c2b8d1d2709
dang pham nhat thanh <dpnthanh@gmail.com>
2017-07-24 11:17:42 +0700

    Click

--- app/src/main/java/com/example/thanh/listview_java/MainActivity.java
+++ app/src/main/java/com/example/thanh/listview_java/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.thanh.listview_java;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     ListView lsName;
     ArrayList<String> listName;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AnhXa();
+        CreateListView();
 
+        lsName.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, listName.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+        lsName.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, listName.get(i) + "One Piece", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+    }
+
+
+    private void CreateListView() { //Khởi tạo ListView
         listName = new ArrayList<>();
 
         listName.add("Luffy");
@@ -31,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
         listName.add("Franky");
         listName.add("Brook");
 
-        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listName);
-        lsName.setAdapter(adapter);
+        adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listName);
 
+        lsName.setAdapter(adapter);
     }
 
     private void AnhXa() {
